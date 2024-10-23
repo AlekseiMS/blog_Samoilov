9f609e847649a9b149c2e67ecd79b9d54a52b502
SamoilovAleksei <aleksei>
2024-10-23 14:23:54 +0300

    Редактирование и удаление отзывов на сайте

--- src/main/java/com/itsamoilov/blog_Samoilov/controllers/MainController.java
+++ src/main/java/com/itsamoilov/blog_Samoilov/controllers/MainController.java
@@ -5,10 +5,13 @@ import com.itsamoilov.blog_Samoilov.repo.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -40,6 +43,51 @@ public class MainController {
                              @RequestParam String text,Map<String, Object> model) {
         Review review = new Review(title, text);
         reviewRepository.save(review);
+
+        return "redirect:/reviews";
+    }
+
+    @GetMapping("/reviews/{id}")
+    public String reviewInfo(@PathVariable(value = "id") long reviewId,
+                             Map<String, Object> model ) {
+        Optional<Review> review = reviewRepository.findById(reviewId);
+
+        ArrayList<Review> result = new ArrayList<>();
+        review.ifPresent(result::add);
+
+        model.put("review", result);
+
+        return "review-info";
+    }
+
+    @GetMapping("/reviews/{id}/update")
+    public String reviewUpdate(@PathVariable(value = "id") long reviewId, Map<String, Object> model) {
+        Optional<Review> review = reviewRepository.findById(reviewId);
+
+        ArrayList<Review> result = new ArrayList<>();
+        review.ifPresent(result::add);
+
+        model.put("review", result);
+        return "review-update";
+    }
+    @PostMapping("/reviews/{id}/update")
+    public String reviewsUpdateForm(@PathVariable(value = "id") long reviewId
+            , @RequestParam String title
+            , @RequestParam String text, Map<String, Object> model) throws ClassNotFoundException {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ClassNotFoundException());
+        review.setTitle(title);
+        review.setText(text);
+        reviewRepository.save(review);
+
+        return "redirect:/reviews/" + reviewId;
+    }
+    @PostMapping("/reviews/{id}/delete")
+    public String reviewsDelete(@PathVariable(value = "id") long reviewId
+            , Map<String, Object> model) throws ClassNotFoundException {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ClassNotFoundException());
+        reviewRepository.delete(review);
 
         return "redirect:/reviews";
     }
